0300c4a1f00ef451fe8f47e0714c76a077330963
Sjors van Holst <sjorsvanholst@gmail.com>
2019-04-12 00:02:43 +0200

    Made main text area resizeable

--- Krijn-Text-4/Editor.Designer.cs
+++ Krijn-Text-4/Editor.Designer.cs
@@ -213,6 +213,9 @@
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.textArea);
